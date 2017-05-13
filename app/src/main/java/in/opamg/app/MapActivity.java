5c4ae7d4ebb62171622603e77eb96b336f072a59
lingeswrn <lingeswrn@gmail.com>
2017-05-13 09:21:55 +0530

    measurement added

--- app/src/main/java/in/opamg/app/MapActivity.java
+++ app/src/main/java/in/opamg/app/MapActivity.java
@@ -41,9 +41,6 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
     boolean network_enabled = false;
     private GoogleMap mMap;
     protected GoogleApiClient mGoogleApiClient;
-    private int Zone;
-    private char Letter;
-    private double Easting, Northing;
 
     /** Called when the activity is first created. */
     @Override
@@ -191,114 +188,16 @@ public class MapActivity extends FragmentActivity implements LocationListener, O
         mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
-                go( mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude());
-                demo( mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude() );
-                //return false;
+                Intent intent = new Intent(MapActivity.this, AddMeasurement.class);
+                intent.putExtra("Latitude", ""+latitude);
+                intent.putExtra("Longitude", ""+longitude);
+                startActivity(intent);
                 return false;
             }
         });
     }
 
-    public void go(double latitude, double longitude){
-//        Intent intent = new Intent(MapsActivity.this, LatiiiLong.class);
-//        intent.putExtra("Latitude", ""+latitude);
-//        intent.putExtra("Longitude", ""+longitude);
-//        startActivity(intent);
-        StringBuilder builder = new StringBuilder();
-
-        if (latitude < 0) {
-            builder.append("S ");
-        } else {
-            builder.append("N ");
-        }
-
-        String latitudeDegrees = Location.convert(Math.abs(latitude), Location.FORMAT_SECONDS);
-        String[] latitudeSplit = latitudeDegrees.split(":");
-        builder.append(latitudeSplit[0]);
-        builder.append("°");
-        builder.append(latitudeSplit[1]);
-        builder.append("'");
-        builder.append(latitudeSplit[2]);
-        builder.append("\"");
-
-        builder.append(" ");
-
-        if (longitude < 0) {
-            builder.append("W ");
-        } else {
-            builder.append("E ");
-        }
-
-        String longitudeDegrees = Location.convert(Math.abs(longitude), Location.FORMAT_SECONDS);
-        String[] longitudeSplit = longitudeDegrees.split(":");
-        builder.append(longitudeSplit[0]);
-        builder.append("°");
-        builder.append(longitudeSplit[1]);
-        builder.append("'");
-        builder.append(longitudeSplit[2]);
-        builder.append("\"");
-
-        Log.e("eee",builder.toString());
-    }
-
-    private void demo(double Lat, double Lon)
-    {
-        Zone= (int) Math.floor(Lon/6+31);
-
-        Log.e("Zone", String.valueOf(Zone));
-        if (Lat<-72)
-            Letter='C';
-        else if (Lat<-64)
-            Letter='D';
-        else if (Lat<-56)
-            Letter='E';
-        else if (Lat<-48)
-            Letter='F';
-        else if (Lat<-40)
-            Letter='G';
-        else if (Lat<-32)
-            Letter='H';
-        else if (Lat<-24)
-            Letter='J';
-        else if (Lat<-16)
-            Letter='K';
-        else if (Lat<-8)
-            Letter='L';
-        else if (Lat<0)
-            Letter='M';
-        else if (Lat<8)
-            Letter='N';
-        else if (Lat<16)
-            Letter='P';
-        else if (Lat<24)
-            Letter='Q';
-        else if (Lat<32)
-            Letter='R';
-        else if (Lat<40)
-            Letter='S';
-        else if (Lat<48)
-            Letter='T';
-        else if (Lat<56)
-            Letter='U';
-        else if (Lat<64)
-            Letter='V';
-        else if (Lat<72)
-            Letter='W';
-        else
-            Letter='X';
-        Easting=0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))*0.9996*6399593.62/Math.pow((1+Math.pow(0.0820944379, 2)*Math.pow(Math.cos(Lat*Math.PI/180), 2)), 0.5)*(1+ Math.pow(0.0820944379,2)/2*Math.pow((0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180))/(1-Math.cos(Lat*Math.PI/180)*Math.sin(Lon*Math.PI/180-(6*Zone-183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2)/3)+500000;
-        Easting=Math.round(Easting*100)*0.01;
 
 
-        Log.e("Easting", String.valueOf(Easting));
-        Northing = (Math.atan(Math.tan(Lat*Math.PI/180)/Math.cos((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))-Lat*Math.PI/180)*0.9996*6399593.625/Math.sqrt(1+0.006739496742*Math.pow(Math.cos(Lat*Math.PI/180),2))*(1+0.006739496742/2*Math.pow(0.5*Math.log((1+Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))/(1-Math.cos(Lat*Math.PI/180)*Math.sin((Lon*Math.PI/180-(6*Zone -183)*Math.PI/180)))),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))+0.9996*6399593.625*(Lat*Math.PI/180-0.005054622556*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+4.258201531e-05*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4-1.674057895e-07*(5*(3*(Lat*Math.PI/180+Math.sin(2*Lat*Math.PI/180)/2)+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2))/4+Math.sin(2*Lat*Math.PI/180)*Math.pow(Math.cos(Lat*Math.PI/180),2)*Math.pow(Math.cos(Lat*Math.PI/180),2))/3);
-        if (Letter<'M')
-            Northing = Northing + 10000000;
 
-
-        Northing=Math.round(Northing*100)*0.01;
-        Log.e("Northing", String.valueOf(Northing));
-
-
-    }
 }
